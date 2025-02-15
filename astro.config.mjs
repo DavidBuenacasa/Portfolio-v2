56bb5b1fca4f31d6efcc3fefda05bf4c70e5835b
David Buenacasa <davidbuenacasalopez12@gmail.com>
2025-02-15 22:43:48 +0100

    Pruevba docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
